e1b7f186ace0291c5c60bfa08ff371169680805e
blondie libra <malevolentninja@gmail.com>
2018-04-20 20:22:12 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+const meals = {};
+
+function updatemeals(meals, food) {}
+  meal[food] = lunch: 'Omlette';
+  return meal;
+};
+
+function removeFromMeals(meal, food) {
+  delete playlist[artist]
+
+  return playlist
+}
